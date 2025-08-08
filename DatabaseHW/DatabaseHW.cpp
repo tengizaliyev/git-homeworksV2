e39a41cd4735e276f15a5d97c63e506d6bcc4f8d
Tengiz Aliyev <tengiz.buisness@gmail.com>
2025-08-08 23:01:17 +0400

    Доработка: обработка исключений и возврат результатов поиска

--- DatabaseHW/DatabaseHW.cpp
+++ DatabaseHW/DatabaseHW.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <pqxx/pqxx>
+#include <vector>
+
 using namespace std;
 using namespace pqxx;
+
+struct ClientInfo {
+    int id;
+    string first_name;
+    string last_name;
+    string email;
+    string phone;
+};
 
 class ClientManager {
 private:
@@ -14,13 +24,12 @@ public:
             cout << "Connected to database successfully!" << endl;
         }
         else {
-            cerr << "Can't open database" << endl;
-            exit(1);
+            throw runtime_error("Can't open database");
         }
     }
 
     ~ClientManager() {
-        delete C; // Соединение закроется автоматически
+        delete C;  // Автоматически закроется
     }
 
     void createTables() {
@@ -39,80 +48,97 @@ public:
             );
         )");
         W.commit();
-        cout << "Tables created successfully." << endl;
     }
 
     void addClient(const string& first_name, const string& last_name, const string& email) {
         work W(*C);
-        zview query = zview("INSERT INTO clients (first_name, last_name, email) VALUES ($1, $2, $3);");
-        W.exec(query, std::make_tuple(first_name, last_name, email));
+        W.exec(zview("INSERT INTO clients (first_name, last_name, email) VALUES ($1, $2, $3);"),
+            std::make_tuple(first_name, last_name, email));
         W.commit();
-        cout << "Client added." << endl;
     }
 
     void addPhone(int client_id, const string& phone) {
         work W(*C);
-        zview query = zview("INSERT INTO phones (client_id, phone) VALUES ($1, $2);");
-        W.exec(query, std::make_tuple(client_id, phone));
+        W.exec(zview("INSERT INTO phones (client_id, phone) VALUES ($1, $2);"),
+            std::make_tuple(client_id, phone));
         W.commit();
-        cout << "Phone added." << endl;
     }
 
     void updateClient(int client_id, const string& first_name, const string& last_name, const string& email) {
         work W(*C);
-        zview query = zview("UPDATE clients SET first_name=$1, last_name=$2, email=$3 WHERE id=$4;");
-        W.exec(query, std::make_tuple(first_name, last_name, email, client_id));
+        W.exec(zview("UPDATE clients SET first_name=$1, last_name=$2, email=$3 WHERE id=$4;"),
+            std::make_tuple(first_name, last_name, email, client_id));
         W.commit();
-        cout << "Client updated." << endl;
     }
 
     void deletePhone(int phone_id) {
         work W(*C);
-        zview query = zview("DELETE FROM phones WHERE id=$1;");
-        W.exec(query, std::make_tuple(phone_id));
+        W.exec(zview("DELETE FROM phones WHERE id=$1;"),
+            std::make_tuple(phone_id));
         W.commit();
-        cout << "Phone deleted." << endl;
     }
 
     void deleteClient(int client_id) {
         work W(*C);
-        zview query = zview("DELETE FROM clients WHERE id=$1;");
-        W.exec(query, std::make_tuple(client_id));
+        W.exec(zview("DELETE FROM clients WHERE id=$1;"),
+            std::make_tuple(client_id));
         W.commit();
-        cout << "Client deleted." << endl;
     }
 
-    void findClient(const string& search) {
+    vector<ClientInfo> findClient(const string& search) {
         work W(*C);
-        zview query = zview(R"(
+        zview query = R"(
             SELECT c.id, first_name, last_name, email, phone
             FROM clients c
             LEFT JOIN phones p ON c.id = p.client_id
             WHERE first_name ILIKE $1 OR last_name ILIKE $1 OR email ILIKE $1 OR phone ILIKE $1;
-        )");
+        )";
+
         result R = W.exec(query, std::make_tuple("%" + search + "%"));
 
+        vector<ClientInfo> results;
         for (const auto& row : R) {
-            cout << "ID: " << row["id"].as<int>() << ", Name: "
-                << row["first_name"].as<string>() << " " << row["last_name"].as<string>()
-                << ", Email: " << row["email"].as<string>()
-                << ", Phone: " << (row["phone"].is_null() ? "None" : row["phone"].as<string>()) << endl;
+            results.push_back(ClientInfo{
+                row["id"].as<int>(),
+                row["first_name"].as<string>(),
+                row["last_name"].as<string>(),
+                row["email"].as<string>(),
+                row["phone"].is_null() ? "" : row["phone"].as<string>()
+                });
         }
+
+        return results;
     }
 };
 
 int main() {
-    string conn_str = "host=localhost port=5432 dbname=postgres user=postgres password=Tengiz200303Aliyev";
-    ClientManager manager(conn_str);
+    try {
+        string conn_str = "host=localhost port=5432 dbname=postgres user=postgres password=Tengiz200303Aliyev";
+        ClientManager manager(conn_str);
 
-    manager.createTables();
-    manager.addClient("Tony", "Stark", "ironman@avengers.com");
-    manager.addPhone(1, "1234567890");
-    manager.addPhone(1, "9876543210");
-    manager.updateClient(1, "Anthony", "Stark", "tony@avengers.com");
-    manager.findClient("tony");
-    manager.deletePhone(1);
-    manager.deleteClient(1);
+        manager.createTables();
+        manager.addClient("Tony", "Stark", "ironman@avengers.com");
+        manager.addPhone(1, "1234567890");
+        manager.addPhone(1, "9876543210");
+        manager.updateClient(1, "Anthony", "Stark", "tony@avengers.com");
+
+        vector<ClientInfo> results = manager.findClient("tony");
+
+        for (const auto& client : results) {
+            cout << "ID: " << client.id
+                << ", Name: " << client.first_name << " " << client.last_name
+                << ", Email: " << client.email
+                << ", Phone: " << (client.phone.empty() ? "None" : client.phone)
+                << endl;
+        }
+
+        manager.deletePhone(1);
+        manager.deleteClient(1);
+    }
+    catch (const std::exception& e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
 
     return 0;
 }
